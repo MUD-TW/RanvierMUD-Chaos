4b5d1973e0710d250c6d6d4068ae8ddf0dce1a81
Hsin-Hsiang Peng <hsinspeng@gmail.com>
2021-07-12 16:04:01 +0800

    🎉 create project

---
+++ README.md
@@ -0,0 +1,36 @@
+<!-- Badge for License -->
+<div align="right">
+
+  [![](https://img.shields.io/github/license/MUD-TW/RanvierMUD-Chaos.svg?style=flat-square)](./LICENSE)
+
+</div>
+
+<!-- Logo and Title -->
+<div align="center">
+  <img src="https://i.imgur.com/yoU8Rbc.png" alt="Chaos" height="150px">
+
+# 洪荒世界（The Chaos）
+
+🌌 _An example MUD world based on [RanvierMUD](https://github.com/RanvierMUD/ranviermud) MUD game engine._<br/>
+_基於 [RanvierMUD](https://github.com/RanvierMUD/ranviermud) 遊戲引擎架站的範例遊戲世界_
+
+</div>
+
+## 快速啟動
+
+```bash
+# 下載專案
+$ git clone https://github.com/MUD-TW/RanvierMUD-Chaos.git
+
+# 安裝依賴
+$ cd RanvierMUD-Chaos
+$ npm install
+$ npm run init
+
+# 啟動服務
+$ npm run start
+```
+
+## 授權許可
+
+Licensed under the MIT License, Copyright © 2021-present MUD-TW (Multi-User Dungeon Taiwan).
---
+++ util/init-bundles.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+'use strict';
+
+const cp = require('child_process');
+const fs = require('fs');
+const os = require('os');
+const readline = require('readline');
+
+const gitRoot = cp.execSync('git rev-parse --show-toplevel').toString('utf8').trim();
+process.chdir(gitRoot);
+
+async function prompt() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  return new Promise((resolve, reject) => {
+    rl.question('Do you want to install the example bundles? [Y/n] ', resolve);
+  });
+}
+
+async function main() {
+
+  try {
+    let answer = await prompt();
+
+    if (answer === 'n') {
+      throw 'foo';
+    }
+  } catch (err) {
+    console.log('Done.');
+    process.exit(0);
+  }
+
+  const githubPath = 'https://github.com/ranviermud/';
+  const defaultBundles = [
+    'https://github.com/MUD-TW/bundle-chaos',
+    'https://github.com/RanvierMUD/telnet-networking',
+    'https://github.com/RanvierMUD/websocket-networking',
+  ];
+  const enabledBundles = [];
+
+  const modified = cp.execSync('git status -uno --porcelain').toString();
+  if (modified) {
+    console.warn('You have uncommitted changes. For safety setup-bundles must be run on a clean repository.');
+    process.exit(1);
+  }
+
+  // add each bundle as a submodule
+  for (const bundle of defaultBundles) {
+    const bundlePath = `bundles/${bundle}`;
+    cp.execSync(`npm run install-bundle ${bundle}`);
+  }
+  console.info('Done.');
+
+  console.info('Enabling bundles...');
+  const ranvierJsonPath = __dirname + '/../ranvier.json';
+  const ranvierJson = require(ranvierJsonPath);
+  ranvierJson.bundles = defaultBundles.map(bundle => bundle.replace(/^.+\/([a-z\-]+)$/, '$1'));
+  fs.writeFileSync(ranvierJsonPath, JSON.stringify(ranvierJson, null, 2));
+  console.info('Done.');
+
+  cp.execSync('git add ranvier.json');
+
+  console.info(`
+-------------------------------------------------------------------------------
+Bundles have been installed as submodules. It's recommended that you now run
+the following commands:
+
+  git commit -m "Install bundles"
+
+You're all set! See https://ranviermud.com for guides and API references
+`);
+
+  process.exit(0);
+}
+
+try {
+  main();
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
